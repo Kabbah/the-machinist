9bbaf90cd2dfc9a8f9dd7889717c21dfd4ba1248
Victor Barpp Gomes <bioniclenuparu@gmail.com>
2020-02-01 16:38:42 -0300

    Add Game Controller for scene routing

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+    public static int bestUnlockedStage = 1;
+
+    public static int GetBestUnlockedStage() => bestUnlockedStage;
+    public static void SetBestUnlockedStage(int stage) => bestUnlockedStage = stage;
+    
+    public static void GoToMainMenu() {
+        SceneManager.LoadScene("mainMenu", LoadSceneMode.Single);
+    }
+
+    public static void GoToStage(int num) {
+        if (num <= bestUnlockedStage) {
+            SceneManager.LoadScene("stage" + num.ToString(), LoadSceneMode.Single);
+        }
+    }
+}
