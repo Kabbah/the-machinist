ab459586f9cb9ecb6c5636c3a95386f61326822b
Julio Maisyrovicz <julio.maistrovicz@hotmail.com>
2020-02-01 04:55:55 -0200

    add ladder feature

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour {
 
     public float maxSpeed = 5;
 
+    public bool isOnLadder = false;
+
     private Rigidbody2D rb;
 
     void Start() {
@@ -14,7 +16,20 @@ public class PlayerController : MonoBehaviour {
     }
 
     void Update() {
-        
+    }
+
+    public void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Ladder")) {
+            this.rb.gravityScale = 0.0f;
+            this.isOnLadder = true;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Ladder")) {
+            this.rb.gravityScale = 1.0f;
+            this.isOnLadder = false;
+        }
     }
 
     public void FixedUpdate() {
@@ -25,6 +40,12 @@ public class PlayerController : MonoBehaviour {
         float horizontalMovement = Input.GetAxis("Horizontal");
         if (this.rb.velocity.magnitude < this.maxSpeed) {
             Vector2 movement = new Vector2(horizontalMovement, 0);
+            this.rb.AddForce(this.force * movement);
+        }
+
+        float verticalMovement = Input.GetAxis("Vertical");
+        if (this.isOnLadder && this.rb.velocity.magnitude < this.maxSpeed) {
+            Vector2 movement = new Vector2(0, verticalMovement);
             this.rb.AddForce(this.force * movement);
         }
     }
