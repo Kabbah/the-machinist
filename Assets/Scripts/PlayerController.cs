d5e3a074a1f0b93a09aea84a7f7a96b2fd217f2c
Victor Barpp Gomes <bioniclenuparu@gmail.com>
2020-02-01 02:59:18 -0300

    Add simple player movement with forces

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float force = 1;
+
+    public float maxSpeed = 5;
+
+    private Rigidbody2D rb;
+
+    void Start() {
+        this.rb = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        
+    }
+
+    public void FixedUpdate() {
+        this.move();
+    }
+    
+    private void move() {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        if (this.rb.velocity.magnitude < maxSpeed) {
+            Vector2 movement = new Vector2(horizontalMovement, 0);
+            this.rb.AddForce(this.force * movement);
+        }
+    }
+}
