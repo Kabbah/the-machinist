164306cbb1c4fb7911baba91a14c0a115dcd45ad
Luis Gustavo <lluisgusttavo@gmail.com>
2020-02-01 06:18:04 -0300

    game timer logic

---
+++ Assets/Scripts/timerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timerController : MonoBehaviour
+{
+	public float timer;
+	public Text timerText;
+    public GameObject victoryMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer = timer - Time.deltaTime;
+        timerText.text = "Time: " + timer.ToString();
+
+        if(timer <= 0.0f){
+        	victoryMenu.SetActive(true);
+        }
+
+
+    }
+}
