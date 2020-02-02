9e88a8b76970470648e08692fcc1dcb81b4d49b9
Julio Maisyrovicz <julio.maistrovicz@hotmail.com>
2020-02-02 14:41:51 -0200

    fase 2

---
+++ Assets/Scripts/LevelsConfig/Level2Config.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level2Config : MonoBehaviour, LevelInterface {
+
+    public int getLevelNumber() {
+        return 2;
+    }
+    public Dictionary<int, GameObject> getIncidents() {
+        Dictionary<int, GameObject> incidents = new Dictionary<int, GameObject>();
+        incidents.Add(50, GameObject.Find("furnace1"));
+        incidents.Add(30, GameObject.Find("pipe1"));
+        incidents.Add(10, GameObject.Find("furnace1"));
+        return incidents;
+
+    }
+
+    public int getLevelTime() {
+        return 60;
+    }
+}
