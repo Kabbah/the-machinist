6208ee88468ff3bed9ed23dff327bc65c0d8e178
Victor Barpp Gomes <bioniclenuparu@gmail.com>
2020-02-02 11:44:57 -0300

    Steam image and other stuff

---
+++ Assets/Scripts/Tooltip.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tooltip : MonoBehaviour {
+    private GameObject tooltip;
+    public GameObject tooltipPrefab;
+
+    public void Show(float offsetX, float offsetY) {
+        Vector3 position = this.transform.position + new Vector3(offsetX, offsetY);
+        this.tooltip = Instantiate(this.tooltipPrefab, position, Quaternion.identity);
+    }
+
+    public void Hide() {
+        Destroy(this.tooltip);
+    }
+}
