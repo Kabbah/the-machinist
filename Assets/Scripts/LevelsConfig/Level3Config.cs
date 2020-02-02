26a4c0e04f8bd76f2b2c5d3a2f6ea1064939278a
Luis Gustavo <lluisgusttavo@gmail.com>
2020-02-02 14:43:26 -0300

    fase 3

---
+++ Assets/Scripts/LevelsConfig/Level3Config.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level3Config : MonoBehaviour, LevelInterface {
+
+    public int getLevelNumber() {
+        return 3;
+    }
+    public Dictionary<int, GameObject> getIncidents() {
+        Dictionary<int, GameObject> incidents = new Dictionary<int, GameObject>();
+        incidents.Add(24, GameObject.Find("furnace1"));
+        incidents.Add(17, GameObject.Find("furnace2"));
+        incidents.Add(10, GameObject.Find("furnace3"));
+        return incidents;
+    }
+
+    public int getLevelTime() {
+        return 25;
+    }
+}
