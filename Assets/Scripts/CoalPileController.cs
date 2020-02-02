70c696f525565c51627a204ea31391f37014e49e
Victor Barpp Gomes <bioniclenuparu@gmail.com>
2020-02-02 13:16:11 -0300

    tooltips display

---
+++ Assets/Scripts/CoalPileController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoalPileController : MonoBehaviour {
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Player")) {
+            transform.Find("coalTooltip").gameObject.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision) {
+        if (collision.CompareTag("Player")) {
+            transform.Find("coalTooltip").gameObject.SetActive(false);
+        }
+    }
+
+}
