a1d57d168d50632d6fe54e4c2a51af6558f03d8c
Victor Barpp Gomes <bioniclenuparu@gmail.com>
2020-02-02 14:45:34 -0300

    fase 4

---
+++ Assets/Scripts/LevelsConfig/Level4Config.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level4Config : MonoBehaviour, LevelInterface {
+
+    public int getLevelNumber() {
+        return 4;
+    }
+    public Dictionary<int, GameObject> getIncidents() {
+        Dictionary<int, GameObject> incidents = new Dictionary<int, GameObject>();
+        incidents.Add(28, GameObject.Find("valve1"));
+        incidents.Add(22, GameObject.Find("furnace1"));
+        incidents.Add(15, GameObject.Find("valve1"));
+        return incidents;
+    }
+
+    public int getLevelTime() {
+        return 30;
+    }
+}
